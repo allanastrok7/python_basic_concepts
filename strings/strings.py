56b981a053264f5678c16d356e3698bb95031f2a
allanastrok7 <allanalekey7@gmail.com>
2024-10-13 16:22:08 +0300

    added a new file

---
+++ strings/strings.py
@@ -0,0 +1,26 @@
+# A deviation from learning python; thebook. I want to taste the waters of the official python documantation the tutorial section
+# starting with the tutorials section 
+# Will be throwing in some qoutes or monolouges from the Game of trones show, so look out for them will have the #Got hashtag
+s='First line\nSecond line' #create a breakup in the line.
+print(s) 
+print(r'C:\python_work\name') #use the raw string {'r'} incase you come across an instance where you want to print the n\ as characters in text.
+print("""
+Usage:thingy[Options]
+    -h             Display this usage messsage  
+    -H hostname    Hostname to connect to    
+""")  #use three quotes if you want an output that comes like that
+
+print("""
+                HElloHelloHellohello
+                   HelloHelloHello
+                       Hello
+""")
+s='mother'
+n='fucker'
+print(s+n) #adding two strings
+
+print('"You\'re going to die tommowrow lord bolton, Sleep well" said Sansa stark to the ripper(lord bolton)') #GOT 
+#############################################################################################################################################################################################################
+
+
+
